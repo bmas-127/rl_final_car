15fe77845469c0efefcb19fc5e569a883c3a8006
bmas-i27 <bcodas@gmail.com>
2023-12-22 03:04:23 +0800

    env half

--- src/environment_wrapper/Env.py
+++ src/environment_wrapper/Env.py
@@ -13,117 +13,94 @@ from PIL import Image, ImageDraw, ImageFont
 from racecar_gym.env import RaceEnv
 import gymnasium as gym
 
-#
-SERVER_RAISE_EXCEPTION = True
-
-# Time unit: second
-MAX_ACCU_TIME = -1
-
-#
-env, reward, trunc, info = None, None, None, None
-obs: Optional[np.array] = None
-sid: Optional[str] = None
-output_freq: Optional[int] = None
-terminal = False
-images = []
-step = 0
-host: Optional[str] = None
-scenario: Optional[str] = None
-
-#
-accu_time = 0.
-last_get_obs_time: Optional[float] = None
-end_time: Optional[float] = None
 
 
-def get_img_views(env):
-    global obs, sid, info
+# ****************************
+# accu_time needed to be added
+# ****************************
+class Env():
+    def __init__(self, scenario, output_freq):
+        self.MAX_ACCU_TIME = -1
+        self.output_freq = config["output_freq"]
+        
+        scenario = config["scenario"]
+        if 'austria' in scenario:
+            self.MAX_ACCU_TIME = 900
+        else:
+            self.MAX_ACCU_TIME = 600
+            
+        self.reset()
+        self.output_freq = output_freq
+        
+    def reset(self):
+        self.images = []
+        self.step = 0
+        self.accu_time = 0
+    
+    def get_img_views(self, env, obs, info):
+        progress = info['progress']
+        lap = int(info['lap'])
+        score = lap + progress - 1.
 
-    progress = info['progress']
-    lap = int(info['lap'])
-    score = lap + progress - 1.
+        # Get the images
+        img1 = env.env.force_render(render_mode='rgb_array_higher_birds_eye', width=540, height=540,
+                                    position=np.array([4.89, -9.30, -3.42]), fov=120)
+        img2 = env.env.force_render(render_mode='rgb_array_birds_eye', width=270, height=270)
+        img3 = env.env.force_render(render_mode='rgb_array_follow', width=128, height=128)
+        img4 = (obs.transpose((1, 2, 0))).astype(np.uint8)
 
-    # Get the images
-    img1 = env.env.force_render(render_mode='rgb_array_higher_birds_eye', width=540, height=540,
-                                position=np.array([4.89, -9.30, -3.42]), fov=120)
-    img2 = env.env.force_render(render_mode='rgb_array_birds_eye', width=270, height=270)
-    img3 = env.env.force_render(render_mode='rgb_array_follow', width=128, height=128)
-    img4 = (obs.transpose((1, 2, 0))).astype(np.uint8)
+        # Combine the images
+        img = np.zeros((540, 810, 3), dtype=np.uint8)
+        img[0:540, 0:540, :] = img1
+        img[:270, 540:810, :] = img2
+        img[270 + 10:270 + 128 + 10, 540 + 7:540 + 128 + 7, :] = img3
+        img[270 + 10:270 + 128 + 10, 540 + 128 + 14:540 + 128 + 128 + 14, :] = img4
 
-    # Combine the images
-    img = np.zeros((540, 810, 3), dtype=np.uint8)
-    img[0:540, 0:540, :] = img1
-    img[:270, 540:810, :] = img2
-    img[270 + 10:270 + 128 + 10, 540 + 7:540 + 128 + 7, :] = img3
-    img[270 + 10:270 + 128 + 10, 540 + 128 + 14:540 + 128 + 128 + 14, :] = img4
+        # Draw the text
+        img = Image.fromarray(img)
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype('./racecar_gym/Arial.ttf', 25)
+        font_large = ImageFont.truetype('./racecar_gym/Arial.ttf', 35)
+        draw.text((5, 5), "Full Map", font=font, fill=(255, 87, 34))
+        draw.text((550, 10), "Bird's Eye", font=font, fill=(255, 87, 34))
+        draw.text((550, 280), "Follow", font=font, fill=(255, 87, 34))
+        draw.text((688, 280), "Obs", font=font, fill=(255, 87, 34))
+        draw.text((550, 408), f"Lap {lap}", font=font, fill=(255, 255, 255))
+        draw.text((688, 408), f"Prog {progress:.3f}", font=font, fill=(255, 255, 255))
+        draw.text((550, 450), f"Score {score:.3f}", font=font_large, fill=(255, 255, 255))
+        draw.text((550, 500), f"ID tester", font=font_large, fill=(255, 255, 255))
 
-    # Draw the text
-    img = Image.fromarray(img)
-    draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype('./racecar_gym/Arial.ttf', 25)
-    font_large = ImageFont.truetype('./racecar_gym/Arial.ttf', 35)
-    draw.text((5, 5), "Full Map", font=font, fill=(255, 87, 34))
-    draw.text((550, 10), "Bird's Eye", font=font, fill=(255, 87, 34))
-    draw.text((550, 280), "Follow", font=font, fill=(255, 87, 34))
-    draw.text((688, 280), "Obs", font=font, fill=(255, 87, 34))
-    draw.text((550, 408), f"Lap {lap}", font=font, fill=(255, 255, 255))
-    draw.text((688, 408), f"Prog {progress:.3f}", font=font, fill=(255, 255, 255))
-    draw.text((550, 450), f"Score {score:.3f}", font=font_large, fill=(255, 255, 255))
-    draw.text((550, 500), f"ID {sid}", font=font_large, fill=(255, 255, 255))
+        img = np.asarray(img)
 
-    img = np.asarray(img)
-
-    return img
-
-
-def record_video(filename: str):
-    global images
-    height, width, layers = images[0].shape
-    # noinspection PyUnresolvedReferences
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    video = cv2.VideoWriter(filename, fourcc, 30, (width, height))
-    for image in images:
-        video.write(image)
-    cv2.destroyAllWindows()
-    video.release()
-
-
-def get_observation():
-    """Return the 3x128x128"""
-    try:
-        global obs, last_get_obs_time
-
-        # Record time
-        last_get_obs_time = time.time()
-
-        return obs, terminal
-    except Exception as e:
-        if SERVER_RAISE_EXCEPTION:
-            raise e
-        print(e)
-        return jsonify({'error': str(e)})
+        return img
 
 
-def set_action(action, env):
-    try:
-        global obs, reward, terminal, trunc, info, step, output_freq, sid, accu_time
+    def record_video(self, filename: str):
+        height, width, layers = self.images[0].shape
+        # noinspection PyUnresolvedReferences
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        video = cv2.VideoWriter(filename, fourcc, 30, (width, height))
+        for image in self.images:
+            video.write(image)
+        cv2.destroyAllWindows()
+        video.release()
 
-        if terminal:
-            return jsonify({'terminal': terminal})
 
 
-        step += 1
+    def set_action(self, action, env):
+        self.step += 1
         obs, _, terminal, trunc, info = env.step(action)
 
         progress = info['progress']
+        print(info["progress"])
         lap = int(info['lap'])
         score = lap + progress - 1.
         env_time = info['time']
 
         # Print information
-        print_info = f'>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Step: {step} Lap: {info["lap"]}, ' \
-                     f'Progress: {info["progress"]:.3f}, ' \
-                     f'EnvTime: {info["time"]:.3f} ' 
+        print_info = f'>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Step: {self.step} Lap: {info["lap"]}, ' \
+                        f'Progress: {info["progress"]:.3f}, ' \
+                        f'EnvTime: {info["time"]:.3f} ' 
         if info.get('n_collision') is not None:
             print_info += f'Collision: {info["n_collision"]} '
         if info.get('collision_penalties') is not None:
@@ -136,54 +113,41 @@ def set_action(action, env):
         # plt.imshow(obs.transpose(1, 2, 0))
         # plt.show()
 
-        if step % output_freq == 0:
-            img = get_img_views(env)
+        if self.step % self.output_freq == 0:
+            img = self.get_img_views(env, obs, info)
             # plt.imshow(img)
             # plt.show()
-            images.append(img)
+            self.images.append(img)
+            
 
         if terminal:
-            if round(accu_time) > MAX_ACCU_TIME:
-                print(f'[Time Limit Error] Accu time "{accu_time}" violate the limit {MAX_ACCU_TIME} (sec)!')
+            # if round(accu_time) > self.MAX_ACCU_TIME:
+            #     print(f'[Time Limit Error] Accu time "{accu_time}" violate the limit {self.MAX_ACCU_TIME} (sec)!')
             cur_time = datetime.now().strftime("%Y%m%d-%H%M%S")
-            video_name = f'results/{sid}_{cur_time}_env{env_time:.3f}_acc{round(accu_time)}s_score{score:.4f}.mp4'
+            video_name = f'results/tester_{cur_time}_env{env_time:.3f}_acc{round(0)}s_score{score:.4f}.mp4'
             Path(video_name).parent.mkdir(parents=True, exist_ok=True)
-            record_video(video_name)
+            self.record_video(video_name)
             print(f'============ Terminal ============')
             print(f'Video saved to {video_name}!')
             print(f'===================================')
 
         return terminal
-    except Exception as e:
-        if SERVER_RAISE_EXCEPTION:
-            raise e
-        print(e)
-        return jsonify({'error': str(e)})
-
-
-
-
-def get_args():
-    global sid, output_freq, scenario, host, MAX_ACCU_TIME
-    sid = "tester"
-    output_freq = 5
-    scenario = "austria_competition"
-    if 'austria' in scenario:
-        MAX_ACCU_TIME = 900
-    else:
-        MAX_ACCU_TIME = 600
 
 
 
 
 
 class RandomAgent:
-    def __init__(self):
-        self.action_space = gym.spaces.Box(low=np.array([-1, -1]), high=np.array([1, 1]), dtype=np.float32)
-        self.env = RaceEnv(scenario=scenario,
-                  render_mode='rgb_array_birds_eye',
-                  reset_when_collision=True if 'austria' in scenario else False)
+    def __init__(self, config):
+        self.scenario = config["scenario"]
+        self.ENV = Env(scenario=self.scenario, output_freq=config["output_freq"])
 
+        
+        self.action_space = gym.spaces.Box(low=np.array([-1, -1]), high=np.array([1, 1]), dtype=np.float32)
+        self.env = RaceEnv(scenario=self.scenario,
+                  render_mode='rgb_array_birds_eye',
+                  reset_when_collision=True if 'austria' in self.scenario else False)
+        
     def act(self, observation):
         return self.action_space.sample()
     
@@ -197,7 +161,7 @@ class RandomAgent:
             print(action_to_take.shape)
             print(action_to_take)
             # Send an action and receive new observation, reward, and done status
-            terminal = set_action(action_to_take, self.env)
+            terminal = self.ENV.set_action(action_to_take, self.env)
 
 
             if terminal:
@@ -206,8 +170,6 @@ class RandomAgent:
 
 
 if __name__ == '__main__':
-    get_args()
-
     # train env
 
     config = {
@@ -225,9 +187,11 @@ if __name__ == '__main__':
         "update_freq": 3,
         "eval_interval": 10,
         "eval_episode": 10,
+        "scenario" : "austria_competition",
+        "output_freq" : 5
     }
     # agent = CarRacingTD3Agent(config)
     
-    rand_agent = RandomAgent()
+    rand_agent = RandomAgent(config)
 
     rand_agent.train()
