9c808bca9b6a823507db4e5075107369bcbaeab2
bmas-127 <bcodmas@gmail.com>
2023-12-30 17:48:59 +0800

    version1

--- client.sh
+++
@@ -1,1 +0,0 @@
-python client.py --url http://127.0.0.1:33333
--- server.sh
+++
@@ -1,3 +0,0 @@
-# python server.py --port 33333 --sid tester --scenario austria_competition
-
-python train.py --port 33333 --sid tester --scenario austria_competition
