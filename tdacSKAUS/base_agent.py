001baadc8f906829eb6af813c47ddb5ed6aaeda6
bmas-127 <bcodmas@gmail.com>
2024-01-04 18:43:10 +0800

    action stack

---
+++ tdacSKAUS/base_agent.py
@@ -0,0 +1,315 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import os
+import time
+from collections import deque
+from torch.utils.tensorboard import SummaryWriter
+from src.replay_buffer.replay_buffer import ReplayMemory
+from abc import ABC, abstractmethod
+import gymnasium as gym
+import cv2
+
+class GaussianNoise:
+    def __init__(self, dim, mu=None, std=None):
+        self.mu = mu if mu else np.zeros(dim)
+        self.std = np.ones(dim) * std if std else np.ones(dim) * .1
+
+    def reset(self):
+        pass
+
+    def generate(self):
+        return np.random.normal(self.mu, self.std)
+
+class OUNoiseGenerator:
+    def __init__(self, mean, std_dev, theta=0.3, dt=5e-2):
+        self.theta = theta
+        self.dt = dt
+        self.mean = mean
+        self.std_dev = std_dev
+
+        self.x = None
+
+        self.reset()
+
+    def reset(self):
+        self.x = np.zeros_like(self.mean.shape)
+
+    def generate(self):
+        self.x = (self.x
+                  + self.theta * (self.mean - self.x) * self.dt
+                  + self.std_dev * np.sqrt(self.dt) * np.random.normal(size=self.mean.shape))
+
+        return self.x
+
+class TD3BaseAgent(ABC):
+    def __init__(self, config):
+        self.gpu = config["gpu"]
+        self.device = torch.device("cuda" if self.gpu and torch.cuda.is_available() else "cpu")
+        # self.device = "cpu"
+        self.total_time_step = 0
+        self.training_steps = int(config["training_steps"])
+        self.batch_size = int(config["batch_size"])
+        self.warmup_steps = config["warmup_steps"]
+        self.first_steps = config["first_steps"]
+        self.second_steps = config["second_steps"]
+        self.third_steps = config["third_steps"]
+        self.total_episode = config["total_episode"]
+        self.eval_interval = config["eval_interval"]
+        self.eval_episode = config["eval_episode"]
+        self.gamma = config["gamma"]
+        self.tau = config["tau"]
+        self.update_freq = config["update_freq"]
+        self.action_space = gym.spaces.Box(low=np.array([-1, -1]), high=np.array([1, 1]), dtype=np.float32)
+        self.logdir = config["logdir"]
+       
+        self.num_frames =  int(config["frame_stake_num"])
+        self.resized_dim = int(config["resized_dim"])
+        self.frame_stack = []
+        self.velocity_stack = []
+        self.action_stack = []
+        self.method = config["method"]
+        
+        self.replay_buffer = ReplayMemory(int(config["replay_buffer_capacity"]))
+        self.writer = SummaryWriter(config["logdir"])
+
+    @abstractmethod
+    def decide_agent_actions(self, state, sigma=0.0):
+        ### TODO ###
+        # based on the behavior (actor) network and exploration noise
+
+        return NotImplementedError
+    
+    
+    @abstractmethod
+    def masters_agent_actions(self, state, velocity, sigma=0.0):
+        ### TODO ###
+        # based on the behavior (actor) network and exploration noise
+
+        return NotImplementedError
+
+
+    def update(self):
+        # update the behavior networks
+        self.update_behavior_network()
+        # update the target networks
+        if self.total_time_step % self.update_freq == 0:
+            self.update_target_network(self.target_actor_net, self.actor_net, self.tau)
+            self.update_target_network(self.target_critic_net1, self.critic_net1, self.tau)
+            self.update_target_network(self.target_critic_net2, self.critic_net2, self.tau)
+
+    @abstractmethod
+    def update_behavior_network(self):
+        # sample a minibatch of transitions
+        state, action, reward, next_state, done = self.replay_buffer.sample(self.batch_size, self.device)
+
+        ### TODO ###
+        # calculate the loss and update the behavior network
+
+        return NotImplementedError
+
+
+    def process_frame_stack(self, new_frame):
+        preprocessed_frame = cv2.cvtColor(np.transpose(new_frame, (1, 2, 0)), cv2.COLOR_RGB2GRAY)
+        preprocessed_frame = cv2.resize(preprocessed_frame, (self.resized_dim, self.resized_dim))
+
+        self.frame_stack.append(preprocessed_frame)
+        self.frame_stack = self.frame_stack[-self.num_frames:]
+
+        stacked_frames = np.stack(self.frame_stack, axis=0)
+
+        return stacked_frames    
+    
+    def process_velocity_stack(self, new_velocity):
+        self.velocity_stack.append(new_velocity)
+        self.velocity_stack = self.velocity_stack[-self.num_frames:]
+
+        stacked_v = np.stack(self.velocity_stack, axis=0)
+
+        return stacked_v 
+    
+    def process_action_stack(self, new_action):
+        self.action_stack.append(new_action)
+        self.action_stack = self.action_stack[-self.num_frames:]
+
+        stacked_v = np.stack(self.action_stack, axis=0)
+
+        return stacked_v 
+    
+    @staticmethod
+    def update_target_network(target_net, net, tau):
+        # update target network by "soft" copying from behavior network
+        for target, behavior in zip(target_net.parameters(), net.parameters()):
+            target.data.copy_((1 - tau) * target.data + tau * behavior.data)
+
+    def train(self):
+        for episode in range(self.total_episode):
+            total_reward = 0
+            original_reward = 0
+            state, infos = self.env.reset()
+            self.noise.reset()
+
+            preprocessed_frame = cv2.cvtColor(np.transpose(state, (1, 2, 0)), cv2.COLOR_RGB2GRAY)
+            preprocessed_frame = cv2.resize(preprocessed_frame, (self.resized_dim, self.resized_dim))
+            self.frame_stack = [preprocessed_frame] * self.num_frames
+            stacked_frames = self.process_frame_stack(state)
+            
+            self.velocity_stack = [[0.0, 0.0, 0.0, 0.0, 0.0, 0.0]] * self.num_frames
+            stacked_velocity = self.process_velocity_stack([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+            
+            self.action_stack = [[0.0, 0.0]] * self.num_frames
+            stacked_action = self.process_action_stack([0.0, 0.0])
+
+            demo = 1
+            
+            teacher_pos = 0.0
+            max_pos = 0.3
+            for t in range(10000):
+                if self.total_time_step < self.warmup_steps:
+                    # austria from scratch ===========================
+                    action = [0.3, 0.008] + np.random.normal(loc=0, scale=0.05, size=(2))
+                    if t > 1 :
+                        pos = info['progress']
+                        if pos > 0.105 and pos < 0.132:
+                            action[1] += 1.0
+                        if pos > 0.25 and pos < 0.28:
+                            action[1] += -0.17
+                        if pos >= 0.36:
+                            teacher_pos = pos
+                            action = self.masters_agent_actions(stacked_frames, stacked_velocity, sigma=0)
+                else:
+                    sigma = max(0.1*(1 - episode/self.total_episode), 0.01)
+                    if t > 1 :
+                        if episode < 1000:
+                            if self.method == "episode":
+                                if episode % 2 == 0:
+                                    action = self.masters_agent_actions(stacked_frames, stacked_velocity, sigma=sigma)
+                                else:
+                                    action = self.decide_agent_actions(stacked_frames, stacked_action, sigma=sigma)
+                                teacher_pos = pos
+                            elif self.method == "step":
+                                if t % 2 == 0:
+                                    action = self.masters_agent_actions(stacked_frames, stacked_velocity, sigma=sigma)
+                                else:
+                                    action = self.decide_agent_actions(stacked_frames, stacked_action, sigma=sigma)
+                            else:
+                                action0 = self.masters_agent_actions(stacked_frames, stacked_velocity, sigma=sigma)
+                                action1 = self.decide_agent_actions(stacked_frames, stacked_action, sigma=sigma)
+                                action = (action0 + action1) / 2
+                        else:
+                            stu_pos = info['progress']
+                            if stu_pos > max_pos:
+                                max_pos = stu_pos
+                
+                
+                # print(stacked_frames.shape)
+                next_state, reward, terminates, truncates, info, ori = self.env.step(action)
+                next_stacked_frames = self.process_frame_stack(next_state)
+                next_stacked_velocity = self.process_velocity_stack(info["velocity"])
+                next_stacked_action = self.process_action_stack(action)
+                
+                
+                self.replay_buffer.append(stacked_frames, stacked_action, action, [reward/10], next_stacked_frames, next_stacked_action, [int(terminates)])
+                if self.total_time_step >= 200:
+                    self.update()
+
+                self.total_time_step += 1
+                total_reward += reward
+                original_reward += ori
+                stacked_frames = next_stacked_frames
+                stacked_velocity = next_stacked_velocity
+                stacked_action = next_stacked_action
+                
+
+                if (teacher_pos >= max_pos + 0.2) and (self.method == "episode"):
+                    terminates = 1
+                
+                if terminates or truncates:
+                    self.writer.add_scalar('Train/Episode Reward', total_reward, self.total_time_step)
+                    print(
+                        'Step: {}\tEpisode: {}\tLength: {:3d}\tReshaped reward: {:.2f}\tOriginal reward: {:.2f}'
+                        .format(self.total_time_step, episode+1, t, total_reward, original_reward))
+
+                    break
+
+            if (episode+1) % self.eval_interval == 0:
+                # save model checkpoint
+                avg_score = self.evaluate()
+                self.save(os.path.join(self.writer.log_dir, f"model_{self.total_time_step}_{float(avg_score)}.pth"))
+                self.writer.add_scalar('Evaluate/Episode Reward', avg_score, self.total_time_step)
+
+    def evaluate(self):
+        print("==============================================")
+        print("Evaluating...")
+        all_rewards = []
+        for episode in range(self.eval_episode):
+            total_reward = 0
+            state, infos = self.env.reset(True)
+            
+            preprocessed_frame = cv2.cvtColor(np.transpose(state, (1, 2, 0)), cv2.COLOR_RGB2GRAY)
+            preprocessed_frame = cv2.resize(preprocessed_frame, (32, 32))
+            self.frame_stack = [preprocessed_frame] * self.num_frames
+            stacked_frames = self.process_frame_stack(state)
+            
+            self.velocity_stack = [[0.0, 0.0, 0.0, 0.0, 0.0, 0.0]] * self.num_frames
+            stacked_velocity = self.process_velocity_stack([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+            
+            self.action_stack = [[0.0, 0.0]] * self.num_frames
+            stacked_action = self.process_action_stack([0.0, 0.0])
+            
+            original_reward = 0
+            for t in range(10000):
+                action = self.decide_agent_actions(stacked_frames, stacked_action)
+                next_state, reward, terminates, truncates, info, ori = self.env.step(action)
+                total_reward += reward
+            
+                stacked_frames = self.process_frame_stack(next_state)
+                stacked_velocity = self.process_velocity_stack(info["velocity"])
+                stacked_action = self.process_action_stack(action)
+                
+                original_reward += ori
+                if terminates or truncates:
+                    print(
+                        'Episode: {}\tLength: {:3d}\tTotal reward: {:.2f}\tOriginal reward: {:3f}'
+                        .format(episode+1, t, total_reward, original_reward))
+                    all_rewards.append(original_reward)
+                    break
+
+        avg = sum(all_rewards) / self.eval_episode
+        print(f"average score: {avg}")
+        print("==============================================")
+        return avg
+    
+    def load_and_train(self, load_path):
+        self.load(load_path)
+        self.train()
+
+    # save model
+    def save(self, save_path):
+        torch.save(
+                {
+                    'actor': self.actor_net.state_dict(),
+                    'critic1': self.critic_net1.state_dict(),
+                    'critic2': self.critic_net2.state_dict(),
+                }, save_path)
+
+    # load model
+    def load(self, load_path):
+        # checkpoint = torch.load(load_path, map_location=torch.device('cpu'))
+        checkpoint = torch.load(load_path)
+        self.actor_net.load_state_dict(checkpoint['actor'])
+        self.critic_net1.load_state_dict(checkpoint['critic1'])
+        self.critic_net2.load_state_dict(checkpoint['critic2'])
+        
+        
+    def load_teacher_and_train(self, load_path):
+        # checkpoint = torch.load(load_path, map_location=torch.device('cpu'))
+        checkpoint = torch.load(load_path)
+        self.teacher_net.load_state_dict(checkpoint['actor'])
+        self.train()
+
+    # load model weights and evaluate
+    def load_and_evaluate(self, load_path):
+        self.load(load_path)
+        self.evaluate()
+
---
+++ tdacSKEV/td3_agent_CarRacing.py
@@ -0,0 +1,173 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from tdacSKEV.base_agent import TD3BaseAgent
+from tdacSKEV.models.CarRacing_model import ActorNetSimple, CriticNetSimple
+from tdacSKEV.models.Teacher_model import TActorNetSimple
+import random
+from tdacSKEV.base_agent import OUNoiseGenerator, GaussianNoise
+from racecar_gym.env import RaceEnv
+import gymnasium as gym
+from tdacSKEV.environment_wrapper.Env import Env
+
+class CarRacingTD3Agent(TD3BaseAgent):
+    def __init__(self, config):
+        super(CarRacingTD3Agent, self).__init__(config)
+        
+        self.scenario = config["scenario"]
+        self.env = Env(scenario=self.scenario,
+            render_mode='rgb_array_birds_eye',
+            reset_when_collision=True if 'austria' in self.scenario else False,
+            output_freq=config["output_freq"],
+            logdir=self.logdir)
+        
+        self.test_env = Env(scenario=self.scenario,
+            render_mode='rgb_array_birds_eye',
+            reset_when_collision=True if 'austria' in self.scenario else False,
+            output_freq=config["output_freq"],
+            logdir=self.logdir)
+    
+        # initialize environment
+        self.observation_space = config["resized_dim"]
+        self.action_space = 2
+        self.action_sample = gym.spaces.Box(low=np.array([-1, -1]), high=np.array([1, 1]), dtype=np.float32)
+        self.feature_space = 6
+
+        
+        # behavior network
+        #print("self.env.observation_space.shape[0]:", self.env.observation_space.shape[0])
+        self.actor_net = ActorNetSimple(self.observation_space, self.action_space, self.action_space, self.num_frames)
+        self.teacher_net = TActorNetSimple(self.observation_space, self.action_space, self.feature_space, self.num_frames)
+        self.critic_net1 = CriticNetSimple(self.observation_space, self.action_space, self.action_space, self.num_frames)
+        self.critic_net2 = CriticNetSimple(self.observation_space, self.action_space, self.action_space, self.num_frames)
+        
+        self.actor_net.to(self.device)
+        self.teacher_net.to(self.device)
+        self.critic_net1.to(self.device)
+        self.critic_net2.to(self.device)
+        
+        # --------------------------
+        # target network
+        self.target_actor_net = ActorNetSimple(self.observation_space, self.action_space, self.action_space, self.num_frames)
+        self.target_critic_net1 = CriticNetSimple(self.observation_space, self.action_space, self.action_space, self.num_frames)
+        self.target_critic_net2 = CriticNetSimple(self.observation_space, self.action_space, self.action_space, self.num_frames)
+        
+        self.target_actor_net.to(self.device)
+        self.target_critic_net1.to(self.device)
+        self.target_critic_net2.to(self.device)
+        self.target_actor_net.load_state_dict(self.actor_net.state_dict())
+        self.target_critic_net1.load_state_dict(self.critic_net1.state_dict())
+        self.target_critic_net2.load_state_dict(self.critic_net2.state_dict())
+
+        # set optimizer
+        self.lra = config["lra"]
+        self.lrc = config["lrc"]
+
+        self.actor_opt = torch.optim.Adam(self.actor_net.parameters(), lr=self.lra)
+        self.critic_opt1 = torch.optim.Adam(self.critic_net1.parameters(), lr=self.lrc)
+        self.critic_opt2 = torch.optim.Adam(self.critic_net2.parameters(), lr=self.lrc)
+
+        # choose Gaussian noise or OU noise
+
+        # noise_mean = np.full(self.env.action_space.shape[0], 0.0, np.float32)
+        # noise_std = np.full(self.env.action_space.shape[0], 2.0, np.float32)
+        # self.noise = OUNoiseGenerator(noise_mean, noise_std)
+        self.noise = GaussianNoise(self.action_space, 0.0, 2.0)
+
+
+    def decide_agent_actions(self, state, stacked_action, sigma=0.0, brake_rate=0.015): 
+        ### TODO ###
+        
+        with torch.no_grad():   
+            state = torch.from_numpy(state).float().to(self.device)
+            stacked_action = torch.from_numpy(stacked_action).float().to(self.device)
+            state = state.unsqueeze(0) 
+            stacked_action = stacked_action.unsqueeze(0) 
+            
+            action = self.actor_net(state, stacked_action).cpu().data.numpy() + sigma * self.noise.generate()
+            action[0, 0] = np.clip(action[0, 0], -1.0, 1.0)  
+            action[0, 1] = np.clip(action[0, 1], -1.0, 1.0)   
+
+            action = action[0] 
+        
+        return action
+    
+    def masters_agent_actions(self, state, velocity, sigma=0.0): 
+        ### TODO ###
+        
+        with torch.no_grad():   
+            state = torch.from_numpy(state).float().to(self.device)
+            velocity = torch.from_numpy(velocity).float().to(self.device)
+            state = state.unsqueeze(0) 
+            velocity = velocity.unsqueeze(0)
+            
+            action = self.teacher_net(state, velocity).cpu().data.numpy() + sigma * self.noise.generate()
+            action[0, 0] = np.clip(action[0, 0], -1.0, 1.0)  
+            action[0, 1] = np.clip(action[0, 1], -1.0, 1.0)   
+
+            action = action[0] 
+        
+        return action
+
+        
+
+    def update_behavior_network(self):
+        # sample a minibatch of transitions
+        state, stacked_action, action, reward, next_state, next_stacked_action, done = self.replay_buffer.sample(self.batch_size, self.device)
+        ### TODO ###
+        
+        #---------------------------------- start
+        
+        ## Update Critic ##
+        # critic loss
+        q_value1 = self.critic_net1(state, stacked_action, action)
+        q_value2 = self.critic_net2(state, stacked_action, action)
+
+        
+        with torch.no_grad():
+            # select action a_next from target actor network and add noise for smoothing
+
+            a_next = self.target_actor_net(next_state, next_stacked_action).cpu().data.numpy() 
+            noise = self.noise.generate()  # generate Gaussian noise
+            
+            # Adding noise and ensuring the action + noise is still within the valid action space
+            a_next = a_next + noise 
+            a_next[0, 0] = np.clip(a_next[0, 0], -1.0, 1.0)  # First action: -1 to +1
+            
+            a_next = torch.from_numpy(a_next).float().to(self.device)
+
+            # get next Q values from target critic networks
+            q_next1 = self.target_critic_net1(next_state, next_stacked_action, a_next)
+            q_next2 = self.target_critic_net2(next_state, next_stacked_action, a_next)
+       
+            # select min q value from q_next1 and q_next2 (double Q learning)
+            q_target = reward + self.gamma * torch.min(q_next1, q_next2) * (1 - done)
+
+        # print(1)
+        
+        # critic loss function
+        criterion = nn.MSELoss()
+        critic_loss1 = criterion(q_value1, q_target)
+        critic_loss2 = criterion(q_value2, q_target)
+
+        # optimize critic
+        self.critic_net1.zero_grad()
+        critic_loss1.backward()
+        self.critic_opt1.step()
+
+        self.critic_net2.zero_grad()
+        critic_loss2.backward()
+        self.critic_opt2.step()
+
+        ## Delayed Actor(Policy) Updates ##
+        if self.total_time_step % self.update_freq == 0:
+            ## Update Actor ##
+            # actor loss
+            action = self.actor_net(state, stacked_action)
+            actor_loss = -1 * self.critic_net1(state, stacked_action, action).mean()
+            # optimize actor
+            self.actor_net.zero_grad()
+            actor_loss.backward()
+            self.actor_opt.step()
+        #----------------------------------  end
+        
